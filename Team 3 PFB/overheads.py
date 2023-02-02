853eefefee7df1c8546304e246b68fe7e46d9c57
Jonas-Lim <s10242074@connect.np.edu.sg>
2023-02-02 20:34:24 +0800

    Done for overhead

---
+++ Team 3 PFB/overheads.py
@@ -0,0 +1,20 @@
+from pathlib import Path
+import csv
+def overheads():
+    highest = [[0, 0]]
+    
+    path = Path.cwd()/"Csv_reports"/"Overheads_day_90.csv"
+
+    with path.open(mode = "r", encoding = "UTF-8", newline = "") as file:
+        reader = csv.reader(file)
+        next(reader)
+
+        for row in reader:
+            if float(row[1]) > float(highest[0][1]):
+                highest[0] = row
+
+    newpath = Path.cwd()/"Summary_report.txt"
+
+    with newpath.open(mode = "a", encoding = "UTF-8") as file:
+        file.write(f"[HIGHEST OVERHEADS] {highest[0][0]}: {highest[0][1]}%\n")
+overheads()
